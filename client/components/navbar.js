5cc57fa51ba696252659782872e0f4e5c9b45e1f
Colleen Risso <colleenrisso@gmail.com>
2019-01-18 14:32:26 -0800

    update(navbar) updating to be MaterialUI compatable

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,32 +1,76 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+const lightColor = 'rgba(255, 255, 255, 0.7)'
+
+const styles = theme => ({
+  secondaryBar: {
+    zIndex: 0
+  },
+  menuButton: {
+    marginLeft: -theme.spacing.unit
+  },
+  iconButtonAvatar: {
+    padding: 4
+  },
+  link: {
+    textDecoration: 'none',
+    color: lightColor,
+    '&:hover': {
+      color: theme.palette.common.white
+    }
+  },
+  button: {
+    borderColor: lightColor
+  }
+})
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tabSelected: 'one'
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = tabSelected => {
+    this.setState({
+      tabSelected: tabSelected
+    })
+    this.props.history.push(tabSelected)
+  }
+
+  render() {
+    const {handleClick, isLoggedIn, classes, onDrawerToggle} = this.props
+    return (
+      <div>
+        <h1>Fortune Teller</h1>
+        <nav>
+          {isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/home">Home</Link>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+            </div>
+          )}
+        </nav>
+        <hr />
+      </div>
+    )
+  }
+}
 
 /**
  * CONTAINER
