095a58c4d449ca5e356db6aa90f880e4587b9073
Colleen Risso <colleenrisso@gmail.com>
2019-01-18 14:18:10 -0800

    add(FormDepreciation) add depreciation form

--- client/components/UserForm.js
+++ client/components/UserForm.js
@@ -1,30 +1,30 @@
 /* eslint-disable default-case */
 import React, {Component} from 'react'
 import FormIncome from './FormIncome'
-// import FormDepreciation from './FormDepreciation'
+import FormDepreciation from './FormDepreciation'
 // import FormBonus from './FormBonus'
 
 export class UserForm extends Component {
   state = {
     step: 1,
     netIncome: 0, //starting point
-    curBonus: 0, //current bonuses paid
-    totalBonus: 0, //total bonuses paid and planned
-    retirement: 0, //retirement, such as a safe harbor
-    otherEmp: 0, //other expenses anticipated for employees/employmeny purposes
+    sigInc: 0, //any unusual income that would not represent the normal course of business if annualized
+    sigExp: 0, //any unusual expense that would not represent the normal course of business if annualized
     curDep: 0, //current depreciation year to date
     curAmort: 0, //current amortization year to date
     totalDep: 0, //total depreciation for the year
     totalAmort: 0, //total amortization for the year
+    curBonus: 0, //current bonuses paid
+    totalBonus: 0, //total bonuses paid and planned
+    retirement: 0, //retirement, such as a safe harbor
+    otherEmp: 0, //other expenses anticipated for employees/employmeny purposes
     curIntExp: 0, //current interest expense year to date
     totalIntExp: 0, //total interest expense anticipated for the year
     curStateTaxes: 0, //current taxes paid for state purposes, year to date
     totalStateTaxes: 0, //total taxes anticipated for the year
     meals: 0, //meals, subject to 50% limitation, must enter at 100%.
     entertainment: 0, //subject to 100% limitation, must enter at 100%
-    nondeductible: 0, //anything that is a fine, penalty, etc. Enter at 100%.
-    sigExp: 0, //any unusual expense that would not represent the normal course of business if annualized
-    sigInc: 0 //any unusual income that would not represent the normal course of business if annualized
+    nondeductible: 0 //anything that is a fine, penalty, etc. Enter at 100%.
   }
 
   // Proceed to next step
@@ -99,13 +99,12 @@ export class UserForm extends Component {
         )
       case 2:
         return (
-          <h1>DEPRECIATION FORM</h1>
-          //   <FormDepreciation
-          //     nextStep={this.nextStep}
-          //     prevStep={this.prevStep}
-          //     handleChange={this.handleChange}
-          //     values={values}
-          //   />
+          <FormDepreciation
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
         )
       case 3:
         return (
