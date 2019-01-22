0d61b1f0de168cc887d897da9a565c922610d03a
Colleen Risso <colleenrisso@gmail.com>
2019-01-22 00:37:48 -0800

    userHome styled

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -9,6 +9,17 @@ import {
 import {me, logout} from '../store'
 import {Link} from 'react-router-dom'
 import {Projection} from './Projection'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  }
+})
 
 export class UserHome extends Component {
   async componentDidMount() {
@@ -20,18 +31,26 @@ export class UserHome extends Component {
     const {email} = this.props
     return (
       <div>
-        <h2>Welcome, {email}</h2>
-        <br />
-        {this.props.project ? (
-          <Projection project={this.props.project} />
-        ) : (
-          <div>
-            <h3>You have not yet created a projection</h3>
-            <Link to="/projection">
-              <button type="button">Create A Projection</button>
-            </Link>
-          </div>
-        )}
+        <Paper>
+          <br />
+          <Paper>
+            <Typography color="primary" variant="h3" component="h3">
+              Welcome, {email}
+            </Typography>
+          </Paper>
+          <br />
+          <br />
+          {this.props.project ? (
+            <Projection project={this.props.project} />
+          ) : (
+            <div>
+              <h3>You have not yet created a projection</h3>
+              <Link to="/projection">
+                <button type="button">Create A Projection</button>
+              </Link>
+            </div>
+          )}
+        </Paper>
 
         {/* <button type="button" onClick={this.props.handleClick()}>
           Logout
